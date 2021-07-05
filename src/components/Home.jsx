40abadbae450dd24635ab99b63a70af0c228ce92
ScriptedRain <71009876+ScriptedRain@users.noreply.github.com>
2021-07-05 15:16:07 +1000

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,24 @@
 import { Typography,  } from '@material-ui/core';
 import React from 'react'
 import WeatherSvg from './imgs/WeatherSvg'
+import { makeStyles } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
 
-
-
+const useStyles = makeStyles((theme) => ({
+    root: {
+        minHeight: '100vh',
+        backgroundImage: './imgs/pexels-josh-hild-2448749.jpg'
+    }
+}))
 
 
 const Home = () => {
     return (
-        <>
-        
+        <div className={classes.root}>
+        <CssBaseline />
         <WeatherSvg />
         <Typography varient='body2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam modi minus recusandae voluptatibus nihil illo natus quod voluptas nam maxime provident libero perspiciatis assumenda, ducimus earum quisquam voluptates? Optio, quia.</Typography>
-        </>
+        </div>
     )
 }
 
