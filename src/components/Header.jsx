58d39554960dc68b5e97ecaa78eb518411749144
ScriptedRain <71009876+ScriptedRain@users.noreply.github.com>
2021-07-05 11:00:07 +1000

    built for prod build

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { AiFillGithub } from 'react-icons/ai'
 import { Pane, Button, Heading, MoonIcon, HomeIcon } from 'evergreen-ui'
+
 const Header = () => {
     return (
         <>
             <Pane display="flex" alignItems="center" marginY={24}>
-                <Heading flex={1}>Jhugnu Weather</Heading>
+                <Heading flex={1}>StarFall Weather</Heading>
                 <Button appearance="minimal" iconAfter={HomeIcon}>Home</Button>
                 <Button iconAfter={MoonIcon}>Weather</Button>
+                <Button iconAfter={<AiFillGithub />} appearance="github">Our Github</Button>
             </Pane>
         </>
     )
---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import { Paragraph } from 'evergreen-ui'
+import React from 'react'
+import WeatherSvg from './imgs/WeatherSvg'
+
+const Home = () => {
+    return (
+        <>
+        <WeatherSvg />
+        <Paragraph size={300} marginTop='default'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam modi minus recusandae voluptatibus nihil illo natus quod voluptas nam maxime provident libero perspiciatis assumenda, ducimus earum quisquam voluptates? Optio, quia.</Paragraph>
+        </>
+    )
+}
+
+export default Home
