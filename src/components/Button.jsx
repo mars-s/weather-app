e9a3534b78b0fe6d1e112dffd7b48e4607a0f023
ScriptedRain <71009876+ScriptedRain@users.noreply.github.com>
2021-07-07 16:48:40 +1000

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = (props) => {
     return (
         
         <button
-            class="bg-blue-500 p-1.5 text-lg px-4 border-b-4 border-blue-800 rounded text-white hover: hover:bg-blue-400 rounded-lg">
+            class="bg-blue-500 p-1.5 text-lg px-4 border-b-4 border-blue-800 text-white hover: hover:bg-blue-400 rounded-lg">
             {props.text}
         </button>
         
