87a30d711725ff9f61747cf847e1ec1f96afbe0f
krishp058 <74632222+krishp058@users.noreply.github.com>
2021-07-21 14:34:04 +1000

    Created About us Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import WeatherMode from './components/WeatherMode'
 import Header from './components/Header'
 import Home from './components/Home'
 import { Switch, Route, BrowserRouter as Router} from 'react-router-dom';
+import TeamSection from './components/TeamSection';
 
 //const apiKey = '25cd6c1f94edbabc663aec7fd32b3bf0'
 
@@ -26,8 +27,7 @@ function App() {
           <Route path='/About us'>
             {/* staff memeber page */}
             <Header currentPage='About us' />
-            <p>This is a collaboratively webapp made my programming students who are learning, This is a trial project that we have
-            created to learn somethings. If there are any inprovements please feel free to tell us about them. </p>
+            <TeamSection/>
           </Route>
         </Switch>
     </Router>
---
+++ src/components/TeamSection.jsx
@@ -0,0 +1,102 @@
+import React from 'react'
+
+const TeamSection = () => {
+    return <div class="p-8 bg-white dark:bg-gray-800 rounded-lg shadow">
+    <p class="text-center text-3xl font-bold text-gray-800 dark:text-white mb-3">
+        Professional team
+    </p>
+    <p class="text-center mb-12 text-xl font-normal text-gray-500 dark:text-gray-200">
+    This is a collaboratively webapp made my programming students who are learning, This is a trial project that we have created to learn somethings. If there are any inprovements please feel free to tell us about them.
+    </p>
+    <div class="flex items-center flex-col md:flex-row ml-20 place-content-center gap-x-1">
+        <div class="p-4https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTleIwDM6fkHty6QW7YkPGWR3F3Ccp10bWehg&usqp=CAU">
+            <div class="text-center mb-4 opacity-90">
+                <a href="#" class="block relative">
+                    <img alt="profile pic" src="https://avatars.githubusercontent.com/u/71009876?v=4" class="mx-auto object-cover rounded-full h-40 w-40 "/>
+                </a>
+            </div>
+            <div class="text-center">
+                <p class="text-2xl text-gray-800 dark:text-white">
+                    ScriptedRain
+                </p>
+                <p class="text-xl text-gray-500 dark:text-gray-200 font-light">
+                    Head Developer
+                </p>
+                <p class="text-md text-gray-500 dark:text-gray-400 max-w-xs py-4 font-light">
+                ScriptedRain is a Head Developer of
+                    <p>
+                        Star Field
+                    </p>
+                </p>
+            </div>
+            <div class="pt-8 flex border-t border-gray-200 w-44 mx-auto text-gray-500 items-center justify-between">
+            </div>
+        </div>
+        <div class="p-4">
+            <div class="text-center mb-4 opacity-90">
+                <a href="#" class="block relative">
+                    <img alt="profile pic" src="https://avatars.githubusercontent.com/u/74632222?s=400&u=39d1044a6e037478cd344024812f2f03df8e2238&v=4" class="mx-auto object-cover rounded-full h-40 w-40 "/>
+                </a>
+            </div>
+            <div class="text-center">
+                <p class="text-2xl text-gray-800 dark:text-white">
+                    Krishp058
+                </p>
+                <p class="text-xl text-gray-500 dark:text-gray-200 font-light">
+                    Co-Head Developer
+                </p>
+                <p class="text-md text-gray-500 dark:text-gray-400 max-w-xs py-4 font-light">
+                    Krishp058 is a Co-Head Developer of
+                    <p>
+                        Star Field
+                    </p>
+                </p>
+            </div>
+            <div class="pt-8 flex border-t border-gray-200 w-44 mx-auto text-gray-500 items-center justify-between">
+            </div>
+        </div>
+        <div class="p-4">
+            <div class="text-center mb-4 opacity-90">
+                <a href="#" class="block relative">
+                    <img alt="profile pic" src="https://avatars.githubusercontent.com/u/83068695?v=4" class="mx-auto object-cover rounded-full h-40 w-40 "/>
+                </a>
+            </div>
+            <div class="text-center">
+                <p class="text-2xl text-gray-800 dark:text-white">
+                    GhostReaper46
+                </p>
+                <p class="text-xl text-gray-500 dark:text-gray-200 font-light">
+                    Front-End Designer
+                </p>
+                <p class="text-md text-gray-500 dark:text-gray-400 max-w-xs py-4 font-light">
+                    GhostReaper46 is a Front-End Designer of Star Field
+                </p>
+            </div>
+            <div class="pt-8 flex border-t border-gray-200 w-44 mx-auto text-gray-500 items-center justify-between">
+            </div>
+        </div>
+        <div class="p-4">
+            <div class="text-center mb-4 opacity-90">
+                <a href="#" class="block relative">
+                    <img alt="profile pic" src="https://cdn.hashnode.com/res/hashnode/image/upload/v1597374204170/yxboJdRi8.jpeg" class="mx-auto object-cover rounded-full h-40 w-40 "/>
+                </a>
+            </div>
+            <div class="text-center">
+                <p class="text-2xl text-gray-800 dark:text-white">
+                    Blazexe Fire
+                </p>
+                <p class="text-xl text-gray-500 dark:text-gray-200 font-light">
+                    Front-End Designer
+                </p>
+                <p class="text-md text-gray-500 dark:text-gray-400 max-w-xs py-4 font-light">
+                Blazexe Fire is a Front-End Designer of Star Field
+                </p>
+            </div>
+            <div class="pt-8 flex border-t border-gray-200 w-44 mx-auto text-gray-500 items-center justify-between">
+            </div>
+        </div>
+    </div>
+</div>
+}
+
+export default TeamSection
