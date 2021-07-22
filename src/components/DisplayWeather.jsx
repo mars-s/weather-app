da4e274284c408f8a37b59f6c126a974f3cb26cd
krishp058 <74632222+krishp058@users.noreply.github.com>
2021-07-22 13:13:44 +1000

    Added weather description

--- src/components/DisplayWeather.jsx
+++ src/components/DisplayWeather.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
 export default function DisplayWeather(props){
     const {json} = props;
     return <div>
@@ -7,8 +11,11 @@ export default function DisplayWeather(props){
             <span className="mt-4 mb-8 text-m text-xl font-extrabold tracking-wider text-center text-gray-900 lg:mt-0 title-font pt-3">
                 {json.name}, {json.sys.country}
             </span>
-        <div className="mt-4 mb-8 text-3xl font-extrabold tracking-widest text-gray-800 text-justify leading-6 lg:mt-0 title-font pt-3">
+            <div className="mt-4 text-3xl font-extrabold tracking-widest text-gray-800 text-justify leading-6 lg:mt-0 title-font pt-3">
                 {Math.round(json.main.temp)}℃
+            </div>
+            <div className="mt-4 mb-8 text-m text-lg font-bold tracking-wider ml-px text-gray-900 lg:mt-0 title-font pt-3">
+                {capitalizeFirstLetter(json.weather[0].description)}
             </div>
         </div>
 
