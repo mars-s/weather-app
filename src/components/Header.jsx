3d9142759db9399554c40b07887ecd49f6349bc7
krishp058 <74632222+krishp058@users.noreply.github.com>
2021-07-22 13:19:34 +1000

    Edited header animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -57,7 +57,7 @@ export default function header(props) {
                         className={classNames(
     
                           item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'px-3 py-2 rounded-md text-sm font-medium', 'transition duration-500 ease-in-out transform hover:scale-110'
+                          'px-3 py-2 rounded-md text-sm font-medium', 'transition duration-300 ease-in-out transform hover:scale-110'
                         )}
                         aria-current={item.current ? 'page' : undefined}
                       >
