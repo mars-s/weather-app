f51bffaa31c512db8ed448def4b8d762ac8e94cf
ScriptedRain <71009876+ScriptedRain@users.noreply.github.com>
2021-07-31 19:52:47 +1000

    made the site more responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+         'moon-pic': "https://images.pexels.com/photos/7766717/pexels-photo-7766717.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+        }),
+    },
   },
   variants: {
     extend: {},
