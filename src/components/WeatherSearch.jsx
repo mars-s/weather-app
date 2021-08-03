84d1a6d523c3b476ff57db002a58af86ef71e3d6
krishp058 <74632222+krishp058@users.noreply.github.com>
2021-08-03 16:13:26 +1000

    Added a searchbar

--- src/components/WeatherSearch.jsx
+++ src/components/WeatherSearch.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import Select from 'react-select';
 import DisplayWeather from './DisplayWeather';
 import options from './Cities';
 
@@ -11,33 +10,25 @@ export default function WeatherSearch() {
     let [Query, setQuery] = useState({})
     //http://openweathermap.org/img/wn/10d@2x.png
 
-    useEffect(() => {
-        if (selectedOption !== 'null') {
-            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${selectedOption.label}&appid=${apiKey}&units=metric`)
+    const search = (event) => {
+        if (event.key === "Enter") {
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${selectedOption}&appid=${apiKey}&units=metric`)
                 .then(Response => Response.json())
                 .then(json => setQuery(
                     {
-                        json : json
+                    json : json
                     }
                 ))
         }
-    }, [selectedOption])
+    }
         
 
     return (
         <div className="container items-center px-5 py-12 lg:px-20">
             <div className="p-6 mx-auto bg-white border rounded-lg shadow-xl lg:w-1/2">
-                <div className='grid grid-cols-4 gap-2'>
-                    <div className='col-span-4 '>
-                        <div className="App">
-                            <Select
-                                defaultValue={selectedOption}
-                                onChange={setSelectedOption}
-                                options={options}
-                            />
-                        </div>
-                    </div>
-
+                <div className="pt-2 relative mx-auto text-gray-600">
+                    <input className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+                      type="search" name="search" placeholder="Enter to Search" onChange={e => setSelectedOption(e.target.value)} onKeyPress={search}/>
                 </div>
                 <div className="flex flex-col items-start py-2 rounded-lg lg:flex-row">
                     <div className="flex flex-col w-full text-blueGray-500 lg:ml-4 p-3">
@@ -57,4 +48,4 @@ export default function WeatherSearch() {
             </div>
         </div>
     );
-}
+};
